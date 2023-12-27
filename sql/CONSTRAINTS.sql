34c0cfd9855c853c87bfa8dfa0d96aac901d5ba3
daersh <daersh@naver.com>
2023-12-27 11:35:25 +0900

    Update CONSTRAINTS.sql

--- sql/CONSTRAINTS.sql
+++ sql/CONSTRAINTS.sql
@@ -1,6 +1,6 @@
 -- ------------------------			CONSTRAINTS			------------------------ --
-
-								-- 1. NOT NULL --
+								   -- 제약 조건 --
+					 			 -- 1. NOT NULL --
 -- NULL을 허용하지 않는 제약 조건 
 DROP TABLE IF EXISTS user_notnull;
 CREATE TABLE IF NOT EXISTS user_notnull (
@@ -23,6 +23,72 @@ SELECT * FROM user_notnull;
 
 
 								  -- 2. UNIQUE --
+-- 중복을 허용하지 않는 제약 조건
+-- Table, insert
+DROP TABLE IF EXISTS user_unique;
+CREATE TABLE IF NOT EXISTS user_unique (
+    user_no INT NOT NULL UNIQUE,   			-- 컬럼 레벨
+    user_id VARCHAR(255) NOT NULL,
+    user_pwd VARCHAR(255) NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    gender VARCHAR(3),
+    phone VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    UNIQUE (phone)    						-- 테이블레벨  
+) ENGINE=INNODB;
+INSERT
+  INTO user_unique
+(user_no, user_id, user_pwd, user_name, gender, phone, email)
+VALUES
+(1, 'user01', 'pass01', '홍길동', '남', '010-1234-5678', 'hong123@gmail.com'),
+(2, 'user02', 'pass02', '유관순', '여', '010-777-7777', 'yu77@gmail.com');
+-- 중복 테스트
+INSERT
+  INTO user_unique
+(user_no, user_id, user_pwd, user_name, gender, phone, email)
+VALUES
+(3, 'user03', 'pass03', '길동', '남', '010-1234-5678', 'hong1234@gmail.com');
+SELECT * FROM user_unique;
+
+
+								-- 3. PRIMARY KEY --
+DROP TABLE IF EXISTS user_primarykey;
+CREATE TABLE IF NOT EXISTS user_primarykey (
+--     user_no INT PRIMARY KEY,
+    user_no INT,
+    user_id VARCHAR(255) NOT NULL,
+    user_pwd VARCHAR(255) NOT NULL,
+    user_name VARCHAR(255) NOT NULL,
+    gender VARCHAR(3),
+    phone VARCHAR(255) NOT NULL,
+    email VARCHAR(255),
+    PRIMARY KEY (user_no)
+) ENGINE=INNODB;
+INSERT 
+  INTO user_primarykey
+(user_no, user_id, user_pwd, user_name, gender, phone, email)
+VALUES
+(1, 'user01', 'pass01', '홍길동', '남', '010-1234-5678', 'hong123@gmail.com'),
+(2, 'user02', 'pass02', '유관순', '여', '010-777-7777', 'yu77@gmail.com');
+SELECT * FROM user_primarykey;      
+
+
+								-- 3. FOREIGN KEY --
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
